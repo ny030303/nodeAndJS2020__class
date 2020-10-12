d3a4835443778ec7278236a695d7542b0287b969
정나영 <ny030303@naver.com>
2020-10-12 10:02:17 +0900

    chat vue commit 1012

---
+++ chat_vue/public/app.js
@@ -0,0 +1,27 @@
+window.onload = function (){
+  window.v = new Vue({
+    el:"#chatApp",
+    mounted(){
+      this.socket = new io();
+    },
+    data: {
+      nickName:'',
+      isLogin:false,
+      socket:null
+    },
+    methods:{
+      login(){
+        if(this.nickName === "") return;
+        this.socket.emit("login", this.nickName);
+
+        this.isLogin = true;
+      },
+      loginOk(){
+
+      },
+      userList(){
+
+      }
+    }
+  })
+}
