395f2d5bd7cf699c4c60af3d3692ffce7f58d4c4
정나영 <ny030303@naver.com>
2020-10-23 23:11:34 +0900

    add chat (vue project) 1023

---
+++ chat/src/components/ChatComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="window">
+      <div class="chat-list">
+
+      </div>
+      <input-component></input-component>
+  </div>
+</template>
+
+<script>
+import InputComponent from './InputComponent';
+export default {
+  name: 'ChatComponent',
+  components:{
+    'input-component':InputComponent
+  }
+}
+</script>
+
+<style scoped>
+  .window {
+    display: grid;
+    grid-template-rows: 1fr 55px;
+  }
+</style>
---
+++ chat/src/components/InputComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="inputBox">
+      <input type="text" class="msg">
+      <button class="btnSend">ENTER</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'InputComponent',
+}
+</script>
+
+<style scoped>
+.inputBox {
+    height:50px;
+    display: flex;
+}
+.msg {
+    flex:1;
+    padding:8px 12px;
+    border:1px solid #ddd;
+    outline:none;
+    font-size: 16px;
+}
+.btnSend {
+    width:109px;
+    background-color: #3780af;
+    border: 0;
+    color: #fff;
+    margin: 0 5px;
+    transition: .4s;
+    outline: none;
+    cursor: pointer;
+}
+
+.btnSend:hover {
+    background-color: #266e9c;
+}
+</style>
